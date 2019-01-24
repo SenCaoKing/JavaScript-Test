aa0b219568b13eb7214da748b1c725c0448da3d1
SenCaoKing <1134349927@qq.com>
2019-01-24 15:12:39 +0800

    JavaScript - 入门篇 - 基本语法 - 标签 示例三 (标签与break、continue配合使用)

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 /* 入门篇 - 基本语法 */
 
-// ④ 注释
+// 4. 注释
 function countdown(n) {
 	while (n --> 0) console.log(n); // 注：n --> 0 会被当做 n-- > 0
 }
@@ -9,7 +9,8 @@ countdown(3);
 // 1
 // 0
 
-// ⑥ switch结构
+
+// 6.3 switch结构
 var x = 1;
 // 注：switch 语句内部采用的是"严格相等运算符(===)"
 switch(x) {
@@ -20,6 +21,24 @@ switch(x) {
 		console.log('x 没有发生类型转换');
 }
 // x 没有发生类型转换
+
+
+// 7.5 标签(label)
+// 标签通常与 break 语句和 continue 语句配合使用，跳出特定的循环。
+top:
+for(var i = 0; i < 3; i++){
+	for(var j = 0; j < 3; j++){
+		if(i === 1 && j === 1) continue top;
+		console.log('i=' + i + ', j=' + j);
+	}
+}
+// i=0, j=0
+// i=0, j=1
+// i=0, j=2
+// i=1, j=0
+// i=2, j=0
+// i=2, j=1
+// i=2, j=2
 
 
 
