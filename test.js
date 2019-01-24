8114c41186bc9cb3f8882c456dea3dfb1d807c88
SenCaoKing <1134349927@qq.com>
2019-01-24 14:41:04 +0800

    JavaScript - 入门篇 - 基本语法 - switch结构 示例二 (switch 语句内部采用的是'格相等运算符(===)')

--- test.js
+++ test.js
@@ -2,9 +2,24 @@
 
 // ④ 注释
 function countdown(n) {
-	while (n --> 0) console.log(n); // n --> 0 会被当做 n-- > 0
+	while (n --> 0) console.log(n); // 注：n --> 0 会被当做 n-- > 0
 }
 countdown(3);
 // 2
 // 1
 // 0
+
+// ⑥ switch结构
+var x = 1;
+// 注：switch 语句内部采用的是"严格相等运算符(===)"
+switch(x) {
+	case true:
+		console.log('x 发生类型转换');
+		break;
+	default:
+		console.log('x 没有发生类型转换');
+}
+// x 没有发生类型转换
+
+
+
