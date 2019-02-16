ae237655642bf73ec6e3e636a79b753fc15b0da6
SenCaoKing <1134349927@qq.com>
2019-02-16 10:13:33 +0800

    JavaScript - 标准库 - Math 对象 - 示例一 (2.9 Math.random() - 返回随机字符串)

--- test.js
+++ test.js
@@ -223,5 +223,25 @@ users
 	});
 
 
+/**
+ * Math 对象
+ */
+
+// 2.9 Math.random() [返回随机字符]
+
+function random_str(length) {
+	var ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	ALPHABET += 'abcdefghijklmnopqrstuvwxyz';
+	ALPHABET += '0123456789-_';
+	var str = '';
+	for (var i = 0; i < length; ++i) {
+		var rand = Math.floor(Math.random() * ALPHABET.length);
+		str += ALPHABET.substring(rand, rand + 1);
+	}
+	return str;
+}
+
+
+
 
 
